ed58074c1c53c634e887a3dc22815e66d995e1a5
Eric-Paul Lecluse <e@epologee.com>
2011-11-13 12:47:28 +0100

    Initial Commit

---
+++ PerfectCrossfade/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  PerfectCrossfade
+//
+//  Created by Eric-Paul Lecluse on 13-11-11.
+//  Copyright (c) 2011 epologee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PerfectCrossfade/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PerfectCrossfade
+//
+//  Created by Eric-Paul Lecluse on 13-11-11.
+//  Copyright (c) 2011 epologee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
